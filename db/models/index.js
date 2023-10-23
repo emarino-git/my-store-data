149fed5c78ec9d797214811995283484a7a00293
emarino-git <marinomatias@outlook.com.ar>
2023-10-23 10:04:44 -0300

    first commit

---
+++ db/models/index.js
@@ -0,0 +1,18 @@
+const { User, UserSchema } = require('./user.model');
+const { CustomerSchema, Customer } = require('./customer.model');
+const { CategorySchema, Category } = require('./category.model');
+const { ProductSchema, Product } = require('./product.model');
+
+function setupModels(sequelize) {
+  User.init(UserSchema, User.config(sequelize));
+  Customer.init(CustomerSchema, Customer.config(sequelize));
+  Category.init(CategorySchema, Category.config(sequelize));
+  Product.init(ProductSchema, Product.config(sequelize));
+
+  User.associate(sequelize.models);
+  Customer.associate(sequelize.models);
+  Category.associate(sequelize.models);
+  Product.associate(sequelize.models);
+}
+
+module.exports = setupModels;
--- services/product.service.js
+++ services/product.service.js
@@ -1,5 +1,7 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
+const { models } = require('../libs/sequelize');
+
 
 class ProductsService {
 
@@ -22,16 +24,15 @@ class ProductsService {
   }
 
   async create(data) {
-    const newProduct = {
-      id: faker.datatype.uuid(),
-      ...data
-    }
-    this.products.push(newProduct);
+    const newProduct = await models.Product.create(data)
     return newProduct;
   }
 
-  find() {
-    return this.products;
+  async find() {
+    const products = await models.Product.findAll({
+      include: ['category']
+    });
+    return products;
   }
 
   async findOne(id) {
