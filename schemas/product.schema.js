71221ec3385cd7a31b439fa0cb62f80f3da35916
emarino-git <marinomatias@outlook.com.ar>
2023-10-23 19:17:38 -0300

    query filters, min, max

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -8,6 +8,8 @@ const image = Joi.string().uri();
 const categoryId = Joi.number().integer();
 const limit = Joi.number().integer();
 const offset = Joi.number().integer();
+const price_min = Joi.number().integer();
+const price_max = Joi.number().integer();
 
 const createProductSchema = Joi.object({
   name: name.required(),
@@ -31,7 +33,13 @@ const getProductSchema = Joi.object({
 
 const queryProductSchema = Joi.object({
   limit,
-  offset
+  offset,
+  price,
+  price_min,
+  price_max: price_max.when('price_min', {
+    is: Joi.number().integer().required(),
+    then: Joi.required()
+  })
 });
 
 module.exports = { createProductSchema, updateProductSchema, getProductSchema, queryProductSchema }
